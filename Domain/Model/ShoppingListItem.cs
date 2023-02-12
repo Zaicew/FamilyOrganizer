d1159ec5647fdb9e75d33e52ed66fa566f257673
Zaicew <danielzajas94@gmail.com>
2023-02-12 21:10:31 +0100

    Add some models and upgrade .gitignore file

---
+++ Domain/Model/ShoppingListItem.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Model;
+
+public class ShoppingListItem : BaseModel
+{
+    public ShoppingListItem(Guid id, string name, int quantity, string note, bool isInTheBasket = false)
+        : base(id)
+    {
+        Name = name;
+        Quantity = quantity;
+        Note = note;
+        IsInTheBasket = isInTheBasket;
+    }
+
+    public int Quantity { get; set; }
+    public string Name { get; set; }
+    public string Note { get; set; }
+    public bool IsInTheBasket { get; set; }
+}

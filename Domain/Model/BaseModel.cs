c111c4d5a4093ffb845d92f7ef4ef385889d751b
Zaicew <danielzajas94@gmail.com>
2023-02-12 19:46:07 +0100

    setup project

---
+++ Domain/Model/BaseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Model;
+
+public class BaseModel
+{
+    public BaseModel(Guid id)
+    {
+        Id = id;
+    }
+
+    public Guid Id { get; }
+}
---
+++ Domain/Model/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace Domain.Model;
+
+public class Category : BaseModel
+{
+    public Category(Guid id)
+        : base(id)
+    {
+
+    }
+
+    public Category()
+    {
+
+    }
+}
